1572e661fe9c808266b3680bb0fd85474a1b74bf
maxmagamedev <batmannolove@gmail.com>
2025-07-30 01:36:00 -0400

    basic Tiled map example

---
+++ level-editor/map/scratch/snake_game_tileset.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="snake_game_tileset" tilewidth="20" tileheight="20" tilecount="32" columns="4">
+ <image source="snake_game_tileset.png" width="80" height="160"/>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="10" y="10">
+    <properties>
+     <property name="type" value="obstacle"/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
